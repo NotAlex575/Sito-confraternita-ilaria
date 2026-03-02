77b71d85f30c4ec7657658a404bd635c6a4d66e5
NotAlex575 <dapontealessandro89gmail.com>
2026-03-02 16:41:41 +0100

    aggiornato problema nella navbar, aggiunto sezione eventi

--- Frontend/SitoConfraternita/components/Navbar.jsx
+++ Frontend/SitoConfraternita/components/Navbar.jsx
@@ -23,223 +23,230 @@ const Navbar = () => {
                 </a>
             </div>
         </div>
-        <div className="d-flex justify-content-between align-items-center">
 
+
+        
+        <div className="d-flex justify-content-between align-items-center">
             {/* LINK GUIDA PER LE PAGINE */}
             <div style={{
                 position: "absolute",
                 left: "50%",
                 transform: "translateX(-50%)"
-            }}>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul
-                        className="menu-header navbar-nav mb-2 mb-lg-0 font-size-header d-flex flex-nowrap"
-                        style={{ whiteSpace: "nowrap" }}
-                    >
-                    {/*  CHI SIAMO */}
-                    <li className="nav-item dropdown">
-                    <a
-                        className="nav-link"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        Chi siamo
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Storia
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Missione
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Associati
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Governo
-                        </a>
-                        </li>
-                    </ul>
-                    </li>
-
-                    {/* L’ARCICONFRATERNITA/MUSEO  */}
-
-                    <li className="nav-item dropdown">
-                    <a
-                        className="nav-link"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        L'arciconfraternita
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Sagrestia
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Oratorio
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Sala delle assemblee
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Quadreria
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Belisario Corenzio
-                        </a>
-                        </li>
-                        <li>
-                        <a className="dropdown-item" href="#">
-                            Terrasanta
-                        </a>
-                        </li>
-                    </ul>
-                    </li>
-
-                    {/* MISSIONE */}
-
-                    <li className="nav-item dropdown">
-                        <a
-                            className="nav-link"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Missione
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Aree d'intervento
-                            </a>
-                            </li>
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Altro....
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    {/* EVENTI / NEWS */}
-
-                    <li className="nav-item dropdown">
-                        <a
-                            className="nav-link"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Eventi / news
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Rassegna stampa
-                            </a>
-                            </li>
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Comunicati
-                            </a>
-                            </li>
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Archivio
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    {/* PERCORSI DI DIDATTICA */}
-
-                    <li className="nav-item dropdown">
-                        <a
-                            className="nav-link"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                        Percorsi di didattica
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Museo per tutti
-                            </a>
-                            </li>
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Altro....
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    {/* VISITE GUIDATE */}
-
-                    <li className="nav-item dropdown">
-                        <a
-                            className="nav-link"
-                            href="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            Visite guidate
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Info e orari
-                            </a>
-                            </li>
-                            <li>
-                            <a className="dropdown-item" href="#">
-                                Archivio
-                            </a>
-                            </li>
-                        </ul>
-                    </li>
-                    </ul>
-                </div>
+            }} className="d-none d-lg-flex">
             </div>
         </div>
+
+
+
+
         {/* MENU HAMBURGER RESPONSIVE DESTRA */}
         <button 
-                className="navbar-toggler" 
-                type="button" 
-                data-bs-toggle="collapse" 
-                data-bs-target="#navbarSupportedContent" 
-                aria-controls="navbarSupportedContent" 
-                aria-expanded="false" 
-                aria-label="Toggle navigation" 
-            > <span className="navbar-toggler-icon"></span>
+            className="navbar-toggler d-lg-none"
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation" 
+            >
+            <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="collapse navbar-collapse d-lg-none" id="navbarSupportedContent">
+            <ul
+                className="menu-header navbar-nav mb-2 mb-lg-0 font-size-header"
+                style={{ whiteSpace: "nowrap" }}
+            >
+            {/*  CHI SIAMO */}
+            <li className="nav-item dropdown">
+            <a
+                className="nav-link"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                Chi siamo
+            </a>
+            <ul className="dropdown-menu">
+                <li>
+                <a className="dropdown-item" href="#">
+                    Storia
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Missione
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Associati
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Governo
+                </a>
+                </li>
+            </ul>
+            </li>
+
+            {/* L’ARCICONFRATERNITA/MUSEO  */}
+
+            <li className="nav-item dropdown">
+            <a
+                className="nav-link"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                L'arciconfraternita
+            </a>
+            <ul className="dropdown-menu">
+                <li>
+                <a className="dropdown-item" href="#">
+                    Sagrestia
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Oratorio
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Sala delle assemblee
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Quadreria
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Belisario Corenzio
+                </a>
+                </li>
+                <li>
+                <a className="dropdown-item" href="#">
+                    Terrasanta
+                </a>
+                </li>
+            </ul>
+            </li>
+
+            {/* MISSIONE */}
+
+            <li className="nav-item dropdown">
+                <a
+                    className="nav-link"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    Missione
+                </a>
+                <ul className="dropdown-menu">
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Aree d'intervento
+                    </a>
+                    </li>
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Altro....
+                    </a>
+                    </li>
+                </ul>
+            </li>
+
+            {/* EVENTI / NEWS */}
+
+            <li className="nav-item dropdown">
+                <a
+                    className="nav-link"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    Eventi / news
+                </a>
+                <ul className="dropdown-menu">
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Rassegna stampa
+                    </a>
+                    </li>
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Comunicati
+                    </a>
+                    </li>
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Archivio
+                    </a>
+                    </li>
+                </ul>
+            </li>
+
+            {/* PERCORSI DI DIDATTICA */}
+
+            <li className="nav-item dropdown">
+                <a
+                    className="nav-link"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                Percorsi di didattica
+                </a>
+                <ul className="dropdown-menu">
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Museo per tutti
+                    </a>
+                    </li>
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Altro....
+                    </a>
+                    </li>
+                </ul>
+            </li>
+
+            {/* VISITE GUIDATE */}
+
+            <li className="nav-item dropdown">
+                <a
+                    className="nav-link"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    Visite guidate
+                </a>
+                <ul className="dropdown-menu">
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Info e orari
+                    </a>
+                    </li>
+                    <li>
+                    <a className="dropdown-item" href="#">
+                        Archivio
+                    </a>
+                    </li>
+                </ul>
+            </li>
+            </ul>
+        </div>
       </div>
     </nav>
     <div style={{paddingBottom: "220px"}}></div>
--- Frontend/SitoConfraternita/src/pages/Homepage.jsx
+++ Frontend/SitoConfraternita/src/pages/Homepage.jsx
@@ -1,12 +1,97 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div className='width-screen'>
-          <img src="/img/1.jpg"alt="" className='width-100per'/>
-          <img src="/img/2.jpg" alt="" className='width-100per'/>
-    </div>
-  )
-}
+    <div className="width-screen">
 
-export default Homepage
+      {/* IMMAGINI HEADER */}
+      <img src="/img/1.jpg" alt="" className="width-100per" />
+      <img src="/img/2.jpg" alt="" className="width-100per" />
+
+      {/* BLOCCO GIALLO TITOLO */}
+      <div className="width-100per bg-primary">
+        <h2 className="text-light text-center mb-0 py-4">
+          EVENTI
+        </h2>
+      </div>
+
+      {/* SEZIONE EVENTI */}
+      <div className="width-100per bg-yellow-light">
+        <div className="container py-5">
+          <h4 className="mb-4">Eventi e News</h4>
+
+          <div className="row g-4">
+
+            {/* CARD 1 */}
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card h-100 shadow">
+                <img
+                  src="/img/eventi/img3.png"
+                  alt=""
+                  className="card-img-top img-fluid"
+                />
+                <div className="card-body text-center">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* CARD 2 */}
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card h-100 shadow">
+                <img
+                  src="/img/eventi/img3.png"
+                  alt=""
+                  className="card-img-top img-fluid"
+                />
+                <div className="card-body text-center">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* CARD 3 */}
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card h-100 shadow">
+                <img
+                  src="/img/eventi/img3.png"
+                  alt=""
+                  className="card-img-top img-fluid"
+                />
+                <div className="card-body text-center">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* CARD 4 */}
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="card h-100 shadow">
+                <img
+                  src="/img/eventi/img3.png"
+                  alt=""
+                  className="card-img-top img-fluid"
+                />
+                <div className="card-body text-center">
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Homepage;
